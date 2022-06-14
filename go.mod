55d15ee5dbeccf47d43bd99a5645cc3cd5b004d6
Milisav Kovacevic <mkovacevic@vast.com>
2022-06-14 20:17:24 +0200

    add unspash routes

--- go.mod
+++ go.mod
@@ -3,13 +3,17 @@ module github.com/GoWebServer/rockets
 go 1.18
 
 require (
-    github.com/esimov/caire v1.4.4
-    github.com/pkg/errors v0.9.1
+	github.com/esimov/caire v1.4.4
+	github.com/pkg/errors v0.9.1
+)
+
+require (
 	gioui.org v0.0.0-20211113093644-40bc2e1f88b8 // indirect
 	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
 	gioui.org/shader v1.0.6 // indirect
 	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/esimov/pigo v1.4.5 // indirect
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3 // indirect
 	golang.org/x/exp v0.0.0-20220317015231-48e79f11773a // indirect
 	golang.org/x/exp/shiny v0.0.0-20220317015231-48e79f11773a // indirect
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
--- rockets/main.go
+++ rockets/main.go
@@ -2,20 +2,37 @@ package main
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 
 	"github.com/GoWebServer/rockets/rockets/seam"
+	"github.com/GoWebServer/rockets/rockets/unsplash"
+	"github.com/flosch/pongo2"
 )
 
 const (
 	IMAGE_URL string = "https://bit.ly/2QGPDkr"
 )
 
+type Task struct {
+	Position int
+	URL      string
+}
+
+type TaskResult struct {
+	Position int
+	Resized  []byte
+	Err      error
+}
+
+var spacexTemplate = pongo2.Must(pongo2.FromFile("spacex.html"))
+
 func main() {
-	fmt.Println("Ready for liftoff! Checkout http://localhost:3000/occupymars")
+	fmt.Println("Ready for liftoff! Checkout \n http://localhost:3000/occupymars \n http://localhost:3000/spacex \n http://localhost:3000/spacex_seams")
+
 	http.HandleFunc("/occupymars", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Query().Get("resize") > "" {
 			resized, err := seam.ContentAwareResize(IMAGE_URL)
@@ -23,11 +40,73 @@ func main() {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+
 			w.Header().Set("Content-Type", "image/jpeg")
 			io.Copy(w, bytes.NewReader(resized))
 		} else {
 			fmt.Fprintf(w, "<html><div>Original image:</div> <img src=\"%s\" /><br/><a href=\"?resize=1\">Resize using Seam Carving</a></html>", IMAGE_URL)
 		}
 	})
+
+	http.HandleFunc("/spacex", func(w http.ResponseWriter, r *http.Request) {
+		response, err := unsplash.LoadRockets()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		err = spacexTemplate.ExecuteWriter(pongo2.Context{"response": response}, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+	})
+
+	http.HandleFunc("/spacex_seams", func(w http.ResponseWriter, r *http.Request) {
+		response, err := unsplash.LoadRockets()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		resultChannel := make(chan TaskResult)
+		taskChannel := make(chan Task)
+		imagesToResize := 8
+
+		// start 4 workers
+		for w := 1; w <= 4; w++ {
+			go worker(w, taskChannel, resultChannel)
+		}
+
+		// write to the taskChannel and close it when we're done
+		go func() {
+			for i, r := range response.Results[:imagesToResize] {
+				taskChannel <- Task{i, r.URLs["small"]}
+			}
+			close(taskChannel)
+		}()
+
+		// start listening for results in a separate goroutine
+		for a := 1; a <= imagesToResize; a++ {
+			taskResult := <-resultChannel
+			if taskResult.Err != nil {
+				log.Printf("Image %d failed to resize", taskResult.Position)
+			} else {
+				sEnc := b64.StdEncoding.EncodeToString(taskResult.Resized)
+				response.Results[taskResult.Position].Resized = sEnc
+			}
+		}
+
+		err = spacexTemplate.ExecuteWriter(pongo2.Context{"response": response}, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func worker(id int, taskChannel <-chan Task, resultChannel chan<- TaskResult) {
+	for j := range taskChannel {
+		fmt.Println("worker", id, "started  job", j.Position)
+		resized, err := seam.ContentAwareResize(j.URL)
+		resultChannel <- TaskResult{j.Position, resized, err}
+	}
 }
